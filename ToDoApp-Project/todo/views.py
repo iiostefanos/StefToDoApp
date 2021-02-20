b3f96a50e9b22b1f12ab88845e7ae1ec27883471
StefanoS <iiostefanos@gmail.com>
2021-02-20 21:00:21 +0000

    created more than half of the app

---
+++ ToDoApp-Project/todo/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout, authenticate
+from .forms import TodoForm
+from .models import ToDo
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+def signupuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+    else:
+        # Create a new user
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+               user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+               user.save()
+               login(request, user)
+               return redirect('currenttodos')
+
+            except IntegrityError:
+               return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Username already taken, please choose another !'})
+        else:
+            return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'The passwords did NOT match'})
+            #Tell the user the passwords didn't match 
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error':'The Username and Password did NOT match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
+
+
+def createtodo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/createtodo.html', {'form':TodoForm()})
+    else:
+        try:
+
+            form = TodoForm(request.POST)
+            newtodo = form.save(commit=False)
+            newtodo.user = request.user
+            newtodo.save()
+            return redirect('currenttodos')
+        except ValueError:
+            return render(request, 'todo/createtodo.html', {'form':TodoForm(),'error':'Title has a max of 150 characters'})
+        
+
+
+def currenttodos(request):
+    todos = ToDo.objects.filter(user=request.user, datecompleted__isnull=True)
+    return render(request, 'todo/currenttodos.html', {'todos':todos})
